e49afa131d695e549357933d973e8f96459c3948
Indu Sree <Indu Sree@DESKTOP-UN8GH21>
2024-08-13 12:55:52 +0530

    Add project files.

---
+++ ClassObject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassObject
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Car myCar = new Car { Brand = "Toyota", Model = "Corolla", Year = 2024 };
+            myCar.Start();
+        }    
+    }
+
+    class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Car has Started");
+        }
+
+    }
+}
